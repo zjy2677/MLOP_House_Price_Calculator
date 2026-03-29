4401fdff370840eae9527a94c6bd7c95469b4f73
Krishuang-1116 <natsuno1116@icloud.com>
2026-03-29 14:47:28 +0200

    Implement anomaly detection in pricing
    Added anomaly detection logic to compare actual price with estimated price and classify as underpriced, overpriced, or normal.

--- backend/services/anomaly.py
+++ backend/services/anomaly.py
@@ -1,1 +1,38 @@
+from services.scoring import calculate_price
+def detect_anomaly(ville: str, actual_price, surface: float, df):
+  '''
+  To detect whether there's anomaly existed, we compare it to our estimation using the following rules:
+  (1) actual price > 1.3 * estimated price -> anomaly overpriced
+  (2) actual price < 0.8 * estimated price -> anomaly underpriced 
+  (3) o.w. we consider it to be normal
+  '''
+  # Two checkers for values and types
+  if not isinstance(actual_price,(int,float)):
+    raise TypeError("Actual price must be a numeric value")
+  if actual_price <= 0:
+    raise ValueError("Actual price must be greater than 0")
+    
+  if not isinstance(actual_price,(int,float)):
+    raise TypeError("Actual price must be a numeric value")
+  if surface <= 0:
+    raise ValueError("Surface area must be greater than 0")
 
+  estimated_price = calculate_price(city, surface, df)
+  lower_bound = 0.8 * estimated_price
+  upper_bound - 1.3 * estimated_price
+  
+  if actual_price < lower_bound:
+        anomaly_type = "anomaly_underprice"
+  elif actual_price > upper_bound:
+        anomaly_type = "anomaly_overprice"
+  else:
+        anomaly_type = "normal"
+  return {
+        "city": city,
+        "surface": surface,
+        "actual_price": actual_price,
+        "estimated_price": estimated_price,
+        "status": anomaly_type
+    }
+
+  
