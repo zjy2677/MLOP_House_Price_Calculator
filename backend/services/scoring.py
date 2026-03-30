df05a8e763fc75da23463a8c08e8362fe9fcd336
zjy2677 <132509394+zjy2677@users.noreply.github.com>
2026-03-30 12:51:56 +0200

    Update scoring.py

--- backend/services/scoring.py
+++ backend/services/scoring.py
@@ -5,18 +5,18 @@ def load_benchmark_data(file_path:str):
   df = pd.read_csv(file_path)
   return df
   
-def get_avg_price(ville: str, df: pd.DataFrame) -> float:
+def get_avg_price(city: str, df: pd.DataFrame) -> float:
   # This function is for retrieving the avg price stored in the database 
   # given the name of the city
-  result = df[df["Commune"] == ville]
+  result = df[df["Commune"] == city]
 
   # This is for preparing the case of input mismatch and user's ivalid input 
   if result.empty:
-    raise ValueError(f"Invalid input for column city:{ville}")
+    raise ValueError(f"Invalid input for column city:{city}")
   return float(result["avg_price_m2"].values[0])
 
   
-def calculate_price(ville: str, surface: float, df: pd.DataFrame) -> float:
+def calculate_price(city: str, surface: float, df: pd.DataFrame) -> float:
   # Caculate the theoretical average price simply using p = surface * avg_price/m^2
   # Check the surface area is indeed a numeric value not like abc
   if not isinstance(surface, (int, float)):
@@ -24,5 +24,5 @@ def calculate_price(ville: str, surface: float, df: pd.DataFrame) -> float:
   # Prevent user invalid input for surface area less than or equal to 0
   if surface <= 0:
     raise ValueError("Surface area should be greater than 0")
-  return get_avg_price(ville,df) * surface 
+  return get_avg_price(city,df) * surface 
   
