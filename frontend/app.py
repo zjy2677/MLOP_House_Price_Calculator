551812d27ee1c446a3b4e85b0ceafb350939f015
zjy2677 <132509394+zjy2677@users.noreply.github.com>
2026-04-02 15:12:08 +0200

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -21,7 +21,7 @@ st.markdown(
     <style>
     .stApp {{
         background:
-        linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),
+        linear-gradient(rgba(0,0,0,0.48), rgba(0,0,0,0.48)),
         url("data:image/png;base64,{base64_img}");
         background-size: cover;
         background-position: center;
