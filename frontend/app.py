0d0b4e792e0e9d2c000a47d5a90a3b3acb42e9ad
zjy2677 <132509394+zjy2677@users.noreply.github.com>
2026-04-02 15:05:12 +0200

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -66,9 +66,18 @@ if st.button("Estimate house price"):
 # Anomaly detection module
 if st.button("Show pricing analysis"):
     result = detect_anomaly(city, actual_price, surface, df)
-    if result['status'] == 'anomaly_overprice':
+
+    if result['status'] == 'extreme_overprice':
+        st.error(f"The estimated price of this house is {estimated_price:.2f}, the actual price {actual_price:.2f} is {ratio:.2f} times of our estimation. This has surpassed 1.6 times benchmark so please be careful that this house is extremely overpriced. We strongly do not recommend you to take any actions on this property")
+
+    elif result['status'] == 'anomaly_overprice':
         st.error(f"The estimated price of this house is {estimated_price:.2f}, the actual price {actual_price:.2f} is {ratio:.2f} times of our estimation. This has surpassed 1.3 times benchmark so please be careful that this house is overpriced. We do not recommend you to take actions on this property")
+
+    elif result['status'] == 'extreme_underprice':
+        st.warning(f"The estimated price of this house is {estimated_price:.2f}, the actual price {actual_price:.2f} is {ratio*100:.2f}% of our estimation. This has dropped below 50% benchmark, please be aware that for some reasons this house is abnormally underpriced. We recommend you to be extremely cautious and do further investigations before any of your decisions")
+
     elif result['status'] == 'anomaly_underprice':
         st.warning(f"The estimated price of this house is {estimated_price:.2f}, the actual price {actual_price:.2f} is {ratio*100:.2f}% of our estimation. This has dropped below 80% benchmark, please be aware that for some reasons this house is underpriced. We recommend you to be cautious when making your decisions")
+
     else:
         st.success(f"This actual price {actual_price:.2f} is {ratio*100:.2f}% of our estimated price {estimated_price:.2f} and stays in acceptable range of 80% - 130%. We thus conclude that this property is priced normally and you may take your desired actions")
